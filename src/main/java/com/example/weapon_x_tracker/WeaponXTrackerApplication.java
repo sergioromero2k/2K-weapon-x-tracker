87e11f9a1c2a77a9a0c0dec6fff5dff4b442b185
Sergio Alejandro <sergioromero2k@gmail.com>
2026-08-22 07:59:50 +0200

    Initial Spring Boot project structure

---
+++ src/main/java/com/example/weapon_x_tracker/WeaponXTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.weapon_x_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeaponXTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeaponXTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/weapon_x_tracker/WeaponXTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weapon_x_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeaponXTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
